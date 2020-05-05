7959ba17e018152ee353fd9ce6679ec034d572d4
Andrew <alo42@georgetown.edu>
2020-05-05 19:00:29 -0400

    saved my lil mistake

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,27 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const result = await fetch
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
   if (result.errors) {
     throw result.errors
