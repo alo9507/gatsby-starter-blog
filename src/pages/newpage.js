2c1b6921775174545d06a62cc8cdee8b95756ebc
Andrew <alo42@georgetown.edu>
2020-05-05 18:56:54 -0400

    added a new page

--- src/pages/newpage.js
+++ src/pages/newpage.js
@@ -2,21 +2,23 @@ import React from "react"
 import { useEffect, useState } from "react"
 
 const NewPage = () => {
-  const [city, setCity] = useState("No City")
-  const [weather, setWeather] = useState("No weather")
+  return <div>IMMA NEW PAGE</div>
 
-  useEffect(() => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=5980a7f999d1d19e632b4d701e68a2cf"
-    )
-      .then(res => res.json())
-      .then(json => {
-        setCity(json.name)
-        setWeather(json.weather[0].description)
-      })
-  })
+  // const [city, setCity] = useState("No City")
+  // const [weather, setWeather] = useState("No weather")
 
-  return <div>{`City: ${city}: Weather: ${weather}`}</div>
+  // useEffect(() => {
+  //   fetch(
+  //     "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=5980a7f999d1d19e632b4d701e68a2cf"
+  //   )
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       setCity(json.name)
+  //       setWeather(json.weather[0].description)
+  //     })
+  // })
+
+  // return <div>{`City: ${city}: Weather: ${weather}`}</div>
 }
 
 export default NewPage
